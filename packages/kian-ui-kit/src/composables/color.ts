059c4fcdf212148f7c4215ad0e1d4b1aad562304
amirkian007 <amircc1200@gmail.com>
2024-04-03 02:17:24 +0330

    feat: ADD new color mixing logic

--- packages/kian-ui-kit/src/composables/color.ts
+++ packages/kian-ui-kit/src/composables/color.ts
@@ -1,15 +1,47 @@
-export function useColor(props: ColorProps) {
-    let colorClasses;
-    let colorStyles;
-    if ((typeof props.color === 'string')&& !props.color.includes('#')) {
-      colorClasses = `text-${props.color}`;
-    } else if (props.color) {
-      colorStyles = {
-        color: props.color
-      };
+type ColorValue = string | null | undefined;
+export function isCssColor(color?: string | null | false): boolean {
+  return !!color && /^(#|var\(--|(rgb|hsl)a?\()/.test(color);
+}
+
+interface colorArgs {
+  background?: ColorValue, text?: ColorValue
+}
+export function useColor(colors:colorArgs) {
+  const classes: string[] = [];
+  const styles: any = {}
+  
+  if (colors.text) {
+    if (isCssColor(colors.text)) {
+      styles.color = colors.text
+    } else {
+      classes.push(`text-${colors.text}`);
     }
-    return { colorClasses, colorStyles };
   }
-  export interface ColorProps {
-    color?: string;
+  if (colors.background) {
+    if (isCssColor(colors.background)) {
+      styles.backgroundColor = colors.background
+     
+    } else {
+      classes.push(`bg-${colors.background}`);
+    }
   }
+
+  return { colorClasses: classes, colorStyles:styles };
+}
+export interface ColorProps {
+  color: string;
+}
+
+export function useTextColors(props:any){
+  {
+    const colors = {
+      text: props.color,
+    }
+  
+    const {
+      colorClasses: textColorClasses,
+      colorStyles: textColorStyles,
+    } = useColor(colors)
+    return { textColorClasses, textColorStyles }
+  }
+}
