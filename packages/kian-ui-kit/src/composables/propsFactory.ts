f6877a952f021097d86dde2ebfbf13d078ee5636
amirkian007 <amircc1200@gmail.com>
2024-04-03 02:18:47 +0330

    refactor: IMPROVE types and add new factories

--- packages/kian-ui-kit/src/composables/propsFactory.ts
+++ packages/kian-ui-kit/src/composables/propsFactory.ts
@@ -1,13 +1,33 @@
+import { ReactNode } from "react";
+import { Varients } from "./varient";
+
 export function propsFactory<T, D>(props: T, defaults: D) {
-  return (providedProps: Partial<D>): T & D => {
-    const mergedProps = { ...defaults, ...props, ...providedProps } as T & D;
-    return mergedProps;
+  type FactoryreturnValue = { componentProps : T & D &{children?:ReactNode} , localProps: Object }
+  return (providedProps: Partial<D>): FactoryreturnValue => {
+    const resolvedProps = { ...defaults, ...props, children:''}
+    const mergedProps = { ...resolvedProps, ...providedProps}
+    return splitProps(mergedProps,Object.keys(resolvedProps as Object)) as FactoryreturnValue;
   };
 }
+export type FactoryreturnValue = ReturnType<typeof propsFactory>
+interface SplitProps{
+   componentProps : Object
+   localProps: Object 
+}
 
-type IconValue = string; // Assuming IconValue is a string for this example
+export function splitProps(props: any, keys: any):SplitProps {
+  const componentProps = {} as any
+  const localProps = { ...props }
+  for (const key of keys) {
+    componentProps[key] = props[key]
+    delete localProps[key]
+  }
+
+  return {componentProps, localProps}
+}
+
 interface Teme {
-  theme: string | undefined;
+  theme: string;
 }
 interface Size {
   size: string | number;
@@ -17,11 +37,15 @@ interface Tag {
 }
 interface Component {
   classList: string[];
+  color:string
+  disabled?:boolean
 }
 // Define helper functions for other prop sets
-export function makeComponentProps(): Component {
+export function makeComponentProps(defautls?:Partial<Component>): Component {
   return {
-    classList: [],
+    classList: defautls?.classList ?? [],
+    color : defautls?.color ?? '',
+    disabled: !!defautls?.disabled
   };
 }
 type Sizes = "x-small"| "small"| "default"| "large"| "x-large"
@@ -35,12 +59,24 @@ export function makeSizeProps(size?: Sizes): Size {
 
 export function makeTagProps(options: { tag: string }): Tag {
   return {
-    tag: "div",
+    tag: options.tag ?? "div",
   };
 }
 
 export function makeThemeProps(): Teme {
   return {
-    theme: undefined,
+    theme: 'light',
   };
+}
+
+export function makeLoaderProps(): {loading:boolean} {
+  return {
+    loading: false,
+  };
+}
+
+export function makeVariantProps(variant?:Varients): {variant:Varients} {
+  return {
+    variant: variant ?? 'elevated',
+  };  
 }
