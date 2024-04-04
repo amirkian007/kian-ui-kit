7451b286c11a29b2b9253ef5e22a0b968f89f405
amirkian007 <amircc1200@gmail.com>
2024-04-04 16:33:39 +0330

    feat: ADD varient hook

---
+++ packages/kian-ui-kit/src/composables/varient.tsx
@@ -0,0 +1,42 @@
+import { useColor } from "./color";
+
+export type Varients =
+  | "flat"
+  | "text"
+  | "elevated"
+  | "tonal"
+  | "outlined"
+  | "plain";
+
+export const allowedVariants = [
+  "elevated",
+  "flat",
+  "tonal",
+  "outlined",
+  "text",
+  "plain",
+] as const;
+
+interface Varienta {
+  variant: Varients;
+  color: string;
+}
+export function useVarient(props: Varienta, name: string) {
+  const variantClasses = `${name}--variant-${props.variant}`;
+  const { colorClasses, colorStyles } = useColor({
+    [["elevated", "flat"].includes(props.variant) ? "background" : "text"]:
+      'hot',
+  });
+
+  return { colorClasses, colorStyles, variantClasses };
+}
+
+export function genOverlays ( name: string) {
+  return (
+    <>
+       <span key="overlay" className={ `${name}__overlay` } /> 
+
+      <span key="underlay" className={ `${name}__underlay` } />
+    </>
+  )
+}
